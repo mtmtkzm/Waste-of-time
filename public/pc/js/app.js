ab458574ae1efb7bd167460340603200692eb55c
松本 和馬 <mtmtkzm00@gmail.com>
2016-03-26 16:57:55 +0900

    frontplateいれた

---
+++ public/pc/js/app.js
@@ -0,0 +1,2 @@
+!function(t){function e(n){if(r[n])return r[n].exports;var i=r[n]={exports:{},id:n,loaded:!1};return t[n].call(i.exports,i,i.exports,e),i.loaded=!0,i.exports}var r={};return e.m=t,e.c=r,e.p="",e(0)}([function(t,e){"use strict";$("#canvas").get(0).width=$(window).width(),$("#canvas").get(0).height=$(window).height()}]);
+//# sourceMappingURL=maps/app.map
---
+++ src/pc/js/app.js
@@ -0,0 +1,9 @@
+// class Main {
+//     constructor() {
+//         // alert();
+//     }
+// }
+// new Main();
+
+$( '#canvas' ).get( 0 ).width = $( window ).width();
+$( '#canvas' ).get( 0 ).height = $( window ).height();
