6b6245c84ff234ad79fc73f3c85363e6df98de82
mtmtkzm <hello@mtmtkzm.com>
2016-03-31 17:27:01 +0900

    mlkccaでランキングデータの送受信ok

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,9 @@
+/* mlkccaの準備
+------------------------------ */
+var milkcocoa = new MilkCocoa('uniimfysaf2.mlkcca.com');
+// データストア（scores）を準備
+var ds = milkcocoa.dataStore('scores');
+
 /* canvasを全画面表示する
 ------------------------------ */
 var ww = $(window).width();
@@ -43,18 +49,15 @@ function startGame () {
 }
 
 function playGame () {
-	// コンテナを準備
     var container = new createjs.Container();
     stage.addChild(container);
 
+    // 変数を宣言
 	var interval = 1500; // 敵生成の間隔
 	var score = 0; // スコア
-
 	var enemy;
 	var enemylife = false; // 敵の命有無
 	var gameover = false; // ゲームオーバーじゃない
-
-	var birthday; // 敵生成の時刻
 
 	// 敵生成（ゲームスタート）
 	createEnemy();
@@ -75,14 +78,12 @@ function playGame () {
 			gameover = true;
 			return; // これ以降createEnemyしない
 		}
-
 		enemy = new createjs.Bitmap(loader.getResult('enemy'));
 		enemy.x = Math.random() * ww - 50;
 		enemy.y = Math.random() * wh - 50;
 		container.addChild(enemy);
 		enemylife = true; // 敵に命あり
 		birthday = new Date(); // 余命を設定する（誕生した時刻）
-
 		enemy.addEventListener('click', removeEnemy);
 		setTimeout(createEnemy, interval);
 	}
@@ -97,22 +98,25 @@ function playGame () {
 
 	// ゲームオーバー判定
  	function judgeGame (event) {
-		if (gameover) {
-			// ゲーム終了処理
-			createjs.Ticker.removeEventListener('tick', judgeGame);
-			stage.removeChild(container);
-			endGame(score);
-		} else {
-			console.log('now Playing.');
-		}
+ 		if (!gameover) { return; }
+		// ゲーム終了処理
+		createjs.Ticker.removeEventListener('tick', judgeGame);
+		stage.removeChild(container);
+		endGame(score);
 	}
 }
 
 function endGame (score) {
+	var name = 'rider';
+	ds.push({name:name, score:score});
 	endScore = new createjs.Text('Your Score\n'+score, 'bold 42px Arial', '#ddd');
 	endScore.x = w/2;
 	endScore.y = h/2;
 	endScore.textAlign = 'center';
 	stage.addChild(endScore); // スコアを描画
-	stage.update();
+	ds.stream().size(5).sort('desc').next(function(err, rank){
+		for (var i=4; i>=0; i--) {
+			console.log(rank[i].value.name+':'+rank[i].value.score);
+		}
+	});
 }
